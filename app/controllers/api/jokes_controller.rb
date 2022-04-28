73e1715ef5a29807262cefa9fc03f3ef85e87f56
Mark-Kohrman <mark.kohrman@gmail.com>
2022-04-27 22:15:22 -0500

    add new api to replace broken api

--- app/controllers/api/jokes_controller.rb
+++ app/controllers/api/jokes_controller.rb
@@ -1,15 +1,26 @@
+require 'uri'
+require 'net/http'
+require 'openssl'
+require 'json'
+
 class Api::JokesController < ApplicationController
-
+  
   def index
-
     render 'index.json.jb'
   end
-
+  
   def show
+    url = URI("https://icanhazdadjoke.com/")
     
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     
-    response = HTTP.get("https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/jokes?key=#{Rails.application.credentials.jokes_api[:api_key]}")
-    @joke = response.parse
+    request = Net::HTTP::Get.new(url)
+    request["Accept"] = "application/json"
+    response = http.request(request)
+
+    @joke = JSON.parse(response.read_body)['joke']
 
     render 'show.json.jb'
   end
